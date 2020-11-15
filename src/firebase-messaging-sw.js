9dfbd365b336f65674327cbe4357b37f14add56d
Cyrill Püntener <34008738+wp99cp@users.noreply.github.com>
2020-11-15 17:45:00 +0100

    fix cloud messaging

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,12 +1,12 @@
 // Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/8.0.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.0.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+
 firebase.initializeApp({
   apiKey: "AIzaSyDi24JB1bcS-HiZfH1nCWqSssME5N49un8",
   authDomain: "finanzaktion-dd819.firebaseapp.com",
@@ -21,16 +21,3 @@ firebase.initializeApp({
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
